e41a5f79703b1373fc7f9ce5a36daba1741cc754
Articdive <13535885+Articdive@users.noreply.github.com>
2022-02-22 15:06:03 +0100

    Attempt to publish to Maven Central.

--- includedFiles/build.gradle.kts
+++ includedFiles/build.gradle.kts
@@ -21,17 +21,47 @@ publishing {
             artifactId = "articdata"
             version = "@mcVersionGit@"
 
+            pom {
+                name.set("ArticData")
+                description.set("Collection of data extracted from Minecraft.")
+                url.set("https://github.com/Articdive/ArticData")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Articdive")
+                        name.set("Articdive")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:github.com/Articdive/ArticData.git")
+                    developerConnection.set("scm:git:ssh://github.com/Articdive/ArticData.git")
+                    url.set("https://github.com/Articdive/ArticData/tree/@mcVersion@")
+                }
+            }
+
             artifact(tasks.getByName("packageVersion"))
         }
     }
     repositories {
         maven {
-            name = "krypton-repo"
-            url = uri("https://repo.kryptonmc.org/releases")
             credentials {
-                username = System.getenv("KRYPTON_REPO_CREDS_USR")
-                password = System.getenv("KRYPTON_REPO_CREDS_PSW")
+                username = System.getenv()["SONATYPE_USERNAME"] ?: (if (hasProperty("SONATYPE_USERNAME")) (property("SONATYPE_USERNAME") as String) else "")
+                password = System.getenv()["SONATYPE_PASSWORD"] ?: (if (hasProperty("SONATYPE_PASSWORD")) (property("SONATYPE_PASSWORD") as String) else "")
             }
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        }
+    }
+    signing {
+        if (System.getenv()["CI"] != null) {
+            useInMemoryPgpKeys(System.getenv()["SIGNING_KEY"], System.getenv()["SIGNING_PASSWORD"])
+            // Only attempt to sign if we are in the CI.
+            // If you are publishing to maven local then it doesn't need signing.
+            sign(publishing.publications["maven"])
         }
     }
 }
