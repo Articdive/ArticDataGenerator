6e64490656fe0e696ec3cc192cdb808e925a1743
Articdive <13535885+Articdive@users.noreply.github.com>
2023-07-12 10:52:49 +0200

    Add Potion Effect Modifiers.

--- DataGenerator/1.17/src/main/java/de/articdive/articdata/generators/v1_17/MobEffectGenerator.java
+++ DataGenerator/1.17/src/main/java/de/articdive/articdata/generators/v1_17/MobEffectGenerator.java
@@ -1,12 +1,14 @@
-package de.articdive.articdata.generators.v1_19_3;
+package de.articdive.articdata.generators.v1_17;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import de.articdive.articdata.datagen.DataGenerator;
 import de.articdive.articdata.datagen.annotations.GeneratorEntry;
-import de.articdive.articdata.generators.v1_19_3.common.DataGenerator_1_19_3;
 import java.lang.reflect.Field;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
@@ -22,7 +24,7 @@ import org.slf4j.LoggerFactory;
 @GeneratorEntry(name = "Color (Decimal)", supported = true)
 @GeneratorEntry(name = "Instanteneous", supported = true)
 @GeneratorEntry(name = "Attribute Modifiers", supported = true)
-public final class MobEffectGenerator extends DataGenerator_1_19_3<MobEffect> {
+public final class MobEffectGenerator extends DataGenerator<MobEffect> {
     private static final Logger LOGGER = LoggerFactory.getLogger(MobEffectGenerator.class);
 
     @Override
@@ -43,17 +45,18 @@ public final class MobEffectGenerator extends DataGenerator_1_19_3<MobEffect> {
 
     @Override
     public JsonObject generate() {
+        List<ResourceLocation> effectRLs = Registry.MOB_EFFECT.keySet().stream().sorted(Comparator.comparingInt(value -> Registry.MOB_EFFECT.getId(Registry.MOB_EFFECT.get(value)))).toList();
         JsonObject effects = new JsonObject();
 
-        for (ResourceLocation effectRL : MOB_EFFECT_REGISTRY.keySet().stream().sorted(Comparator.comparingInt(value -> MOB_EFFECT_REGISTRY.getId(MOB_EFFECT_REGISTRY.get(value)))).toList()) {
-            MobEffect me = MOB_EFFECT_REGISTRY.get(effectRL);
+        for (ResourceLocation effectRL : effectRLs) {
+            MobEffect me = Registry.MOB_EFFECT.get(effectRL);
 
             JsonObject effect = new JsonObject();
             // Null safety check.
             if (me == null) {
                 continue;
             }
-            effect.addProperty("id", MOB_EFFECT_REGISTRY.getId(me));
+            effect.addProperty("id", Registry.MOB_EFFECT.getId(me));
             effect.addProperty("mojangName", names.get(me));
             effect.addProperty("translationKey", me.getDescriptionId());
             effect.addProperty("color", me.getColor());
